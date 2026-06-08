134e5e423a070b2c7e1e0a9ca917d20b610cbf8a
Ignition User <user@ignition.local>
2026-06-08 14:09:03 +0530

    fixed code

--- ignition/script-python/NewScript/code.py
+++ ignition/script-python/NewScript/code.py
@@ -2,7 +2,5 @@ def test():
 	print("data12545")
 	print("testab1cd")
 	print("BabaJiKi145iutyrt0640Booti")
-=======
 	print("Baba40Boot100i")
->>>>>>> branch 'master' of https://github.com/soubhagya2001/Ignition-demo-test.git
 
